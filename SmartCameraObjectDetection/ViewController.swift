f5fe3219a992afa6275ec4a595bacc856f36af28
Satya <asathyakrishna@gmail.com>
2020-08-14 17:37:30 -0400

    Initial Commit

---
+++ SmartCameraObjectDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartCameraObjectDetection
+//
+//  Created by Satya Venkata Krishna Achanta on 8/14/20.
+//  Copyright © 2020 Satya Venkata Krishna Achanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
